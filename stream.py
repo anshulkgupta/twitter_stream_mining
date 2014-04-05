f6610db91a9b8cf48328d8174574aeb3ea3948e1
Anshul Gupta <anshul.dlf@gmail.com>
2014-04-05 14:34:29 -0400

    first test

--- stream.py
+++ stream.py
@@ -31,20 +31,6 @@ def lieswithinIndia(latitude, longitude):
 			return True
 	return False
 
-# Requires ONE hashtag to be in the tweet.
-def hashtag_OR_filter(message):
-	for query in hashtag_queries:
-		if query in message:
-			return True
-	return False
-
-# Requres ALL hashtags to be in the tweet.
-def hashtag_AND_filter(message):
-	for query in hashtag_queries:
-		if query not in message:
-			return False
-	return True
-
 def start_record(db_name):
 	sql_init = """
 	CREATE TABLE if NOT EXISTS {} (
@@ -83,18 +69,10 @@ if __name__ == '__main__':
 
 	query = str(raw_input('Enter the hashtags you would like to search for '
 				+ 'separated by spaces: ')).lower()
-	hashtag_queries = query.split(' ')
+	hashtag_queries = ['ab ki baar modi sarkar']
 
-	search_type = str( raw_input('Should returned tweets include all or >=1 hashtags? Enter '+
-		'"all" or "one"')).lower()
+	db_name = 'download1'
 
-	if 'all' in search_type:
-		hashtag_filter = hashtag_AND_filter
-		db_name = '_AND_'.join(hashtag_queries)
-	else:
-		hashtag_filter = hashtag_OR_filter
-		db_name = '_OR_'.join(hashtag_queries)
-	
 	print db_name
 	db = start_record(db_name)
 
